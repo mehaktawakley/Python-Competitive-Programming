a2447356da461512c6ae23d322b6b7ca390ffb61
Mehak <mehaktawakley@gmail.com>
2018-07-22 00:40:33 +0530

    initial commit

--- Textwrap.py
+++ Textwrap.py
@@ -1,3 +1,28 @@
+"""
+You are given a string S and width w. 
+Your task is to wrap the string into a paragraph of width w.
+
+Input Format
+The first line contains a string, S. 
+The second line contains the width, w.
+
+Output Format
+Print the text wrapped paragraph.
+
+Sample Input:
+ABCDEFGHIJKLIMNOQRSTUVWXYZ
+4
+
+Sample Output:
+ABCD
+EFGH
+IJKL
+IMNO
+QRST
+UVWX
+YZ
+"""
+
 import textwrap
 
 def wrap(string, max_width):
