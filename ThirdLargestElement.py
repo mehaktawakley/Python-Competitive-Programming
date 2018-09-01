225031d8a6f438a5ac461b7726c2df4cd759f6a2
Mehak <mehaktawakley@gmail.com>
2018-09-01 23:24:41 +0530

    init commit
    Question solved from geeksforgeeks Sudo Placement set.

---
+++ ThirdLargestElement.py
@@ -0,0 +1,15 @@
+def thirdLargest(arr, n):
+    if(n<3):
+        return (-1)
+    else:
+        arr.sort()
+        return (arr[-3])
+
+if __name__ == '__main__':
+	t = int(input())
+	for i in range(t):
+		n = int(input())
+		arr = list(map(int,input().strip().split()))
+		print(thirdLargest(arr,n))
+		
+
