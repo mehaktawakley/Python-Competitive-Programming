9fba09a6d297d0d786151b1b4f2ce89d2eebbee0
Mehak <mehaktawakley@gmail.com>
2019-05-02 23:30:41 +0530

    init commit

--- FindingMiddleElementInLinkedList.py
+++ FindingMiddleElementInLinkedList.py
@@ -1,6 +1,5 @@
 """
 Given a singly linked list of N nodes. The task is to find middle of the linked list. For example, if given linked list is 1->2->3->4->5 then output should be 3. 
-
 If there are even nodes, then there would be two middle nodes, we need to print second middle element. For example, if given linked list is 1->2->3->4->5->6 then output should be 4.
 
 Input:
